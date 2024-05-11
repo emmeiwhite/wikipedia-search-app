785c4bcf3f3abd3843d02944c00075c5b8e6ff72
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-12 00:15:08 +0530

    Functionality Working fine!

--- app.js
+++ app.js
@@ -5,35 +5,70 @@ document.addEventListener('DOMContentLoaded', function () {
   // Imperative Approach in Pure JavaScript
   const form = document.querySelector('.form')
   const input = document.querySelector('.form-input')
-  const results = document.querySelector('.results')
+  const resultsDOM = document.querySelector('.results')
 
-  const error = document.querySelector('.error')
+  //   const error = document.querySelector('.error')
 
   form.addEventListener('submit', function (e) {
     e.preventDefault()
 
     if (!input.value) {
-      error.classList.add('show-error')
+      console.log('No value provided')
+      resultsDOM.innerHTML = `<div class="error">Please provide a valid input</div>`
+
       return
     }
-
-    error.classList.remove('show-error')
 
     const searchTerm = input.value
 
     let searchURL = `${url}${searchTerm}`
 
-    input.value = ''
+    // input.value = ''
     //   Make API call
     fetchResults(searchURL)
   })
 
   async function fetchResults(url) {
-    results.innerHTML = `<div class="loading"></div>`
+    resultsDOM.innerHTML = `<div class="loading"></div>`
     try {
-      const response = fetch(url).then(response => {
-        console.log(response)
+      const response = await fetch(url)
+      const data = await response.json()
+
+      const results = data.query.search
+
+      if (results.length < 1) {
+        resultsDOM.innerHTML = '<div class="error"> No matching results. Please try again</div>'
+        return
+      }
+
+      renderResults(results)
+    } catch (error) {
+      console.log('In the Error Block!')
+      resultsDOM.innerHTML = `<div class="error"> There was error loading the page</div>`
+    }
+  }
+
+  // render the results
+
+  function renderResults(list) {
+    console.log(list)
+    const cardList = list
+      .map(item => {
+        const { title, pageid, snippet } = item
+        return `
+          <a
+            href="http://en.wikipedia.org/?curid=${pageid}"
+            target="_blank"
+          >
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>
+     `
       })
-    } catch (error) {}
+      .join('')
+
+    resultsDOM.innerHTML = `<div class="articles">${cardList}</div>`
   }
 })
